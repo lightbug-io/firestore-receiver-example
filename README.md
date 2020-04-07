1d0127f23355e40ff0f7a06a84e1748f37117b50
Chris Guest <chris@thelightbug.com>
2020-04-07 11:01:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,78 @@
+# Firebase HTTPS receiver for Lightbug API quickstart
+
+## Introduction
+
+The function `lbListener` is created to receive HTTP POST messages from LB API.
+
+The URL of this function, eg:
+```
+https://us-central1-AAAA-BBBBB.cloudfunctions.net/lbListener
+```
+Should be used in the register_push example as the endpoint value
+
+Further reading:
+
+ - [Read more about the Firebase SDK for Cloud Functions](https://firebase.google.com/docs/functions)
+
+
+## Initial setup, build tools and dependencies
+
+### 1. Clone this repo
+
+Clone or download this repo and navigate into the directory
+
+### 2. Install the Firebase CLI and enable Functions on your Firebase CLI
+
+You need to have installed the Firebase CLI. If you haven't run:
+
+```bash
+npm install -g firebase-tools
+```
+
+> Doesn't work? You may need to [change npm permissions](https://docs.npmjs.com/getting-started/fixing-npm-permissions).
+
+Login to firebase (if not already done) using
+```bash
+firebase login
+```
+
+### 3. Create a Firebase project and configure the quickstart
+
+Create a Firebase Project on the [Firebase Console](https://console.firebase.google.com).
+
+
+Set up your Firebase project by running `firebase use --add`, select your Project ID and follow the instructions.
+
+
+
+
+## Deploy the app to prod
+
+First you need to install the `npm` dependencies of the functions:
+
+```bash
+cd functions && npm install; cd ..
+```
+
+This installs locally:
+ - The Firebase SDK and the Firebase Functions SDK.
+ - Tracking api deps
+
+Deploy to Firebase using the following command:
+
+```bash
+firebase deploy
+```
+
+This deploys and activates the lbListener Function.
+
+> The first time you call `firebase deploy` on a new project with Functions will take longer than usual.
+
+
+## View results
+
+The example will create collections under /incoming - view data there
+
+## License
+
+© Lightbug Ltd, 2020. Licensed under an [Apache-2](../../LICENSE) license.
---
+++ functions/index.js
@@ -0,0 +1,71 @@
+'use strict';
+
+// [START firebaseimport]
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+const db = admin.firestore();
+// [END firebaseimport]
+// [START additionalimports]
+// Moments library to format dates.
+const moment = require('moment');
+// request to fetch subscription confirmation url
+const request = require('request');
+// [END additionalimports]
+
+
+// [START trigger]
+exports.lbListener = functions.https.onRequest((req, res) => {
+  // [END trigger]
+  // [START sendError]
+  // Optional - Forbidding non POST requests.
+  // if (req.method !== 'POST') {
+  //   return res.status(403).send('Forbidden!');
+  // }
+  // [END sendError]
+
+   const body = JSON.parse(req.body);
+
+   console.log('Incoming data:', body);
+
+   // Confirm we want to receive data
+   if(body.Type === 'SubscriptionConfirmation')
+   {
+       console.log('Fetching subscribe url', body.SubscribeURL);
+       request(body.SubscribeURL, (err, response) => {
+           if (err) {
+               console.log('failed to fetch subscribe url, check that your function has external connectivity or visit ' +
+                   body.SubscribeURL + ' from your browser', err);
+               res.status(500).send('fail');
+           } else {
+               console.log('Fetched subscribe url', response);
+               res.status(200).send('ok');
+           }
+       });
+     return; // stop function here
+   }
+
+   // Once confirmed, we'll receive all notifications
+    if(body.Type === 'Notification')
+    {
+      // ref: https://api.thelightbug.com/docs/#definition-notification
+      const notification = JSON.parse(body.Message);
+      // ref: https://api.thelightbug.com/docs/#definition-datapoint
+      const datapoint = notification.datapoint;
+      // ref: https://api.thelightbug.com/docs/#definition-device
+      const device = notification.device;
+
+      // Example: log entries
+      console.log(`Device #${device.id} has a new point for ${moment(datapoint.timestamp)} at
+                  ${datapoint.location.lat}, ${datapoint.location.lng} - ${datapoint.address}`);
+
+      // Example 2: write to firestore
+        db.collection('incoming/'+device.id+'/datapoints').doc(datapoint.timestamp).set(datapoint);
+    }
+
+    // [START sendResponse]
+    // NOTE - If this script returns anything other than a 2XX (HTTP OK) code, the notification will be retried
+    res.status(200).send('ok');
+    // [END sendResponse]
+});
+// [END all]
